9120328da0b29dbf9a1927684dad3baff6670363
Ashutosh Dave <Ashu@Ashus-Mac-mini.local>
2019-03-03 17:38:05 +0530

    Initial Commit

---
+++ BeaconManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeaconManager
+//
+//  Created by Ashutosh Dave on 03/03/19.
+//  Copyright © 2019 Ashutosh Dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
